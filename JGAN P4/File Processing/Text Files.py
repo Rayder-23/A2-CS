7c3098a78629d4c9ec3ede87f48593c0547fed7f
Rayder <165102003+Rayder-23@users.noreply.github.com>
2025-05-03 05:49:12 +0500

    update text files

--- JGAN P4/File Processing/Text Files.py
+++ JGAN P4/File Processing/Text Files.py
@@ -1,9 +1,9 @@
 # Writing to a text file:
 
 # Open a file in write mode ('w')
-file = open( 'output.txt', 'w')
+file = open('output.txt', 'w')
 # Write a line of text to the file
-file.write( "This is a line of text.")
+file.write("This is a line of text.")
 # Close the file
 file.close()
 
@@ -11,7 +11,7 @@ file.close()
 # Reading from a text file:
 
 # Open a file in read mode ('r')
-file = open ('output. txt', 'r')
+file = open('output.txt', 'r')
 # Read a line of text from the file
 line = file. readline()
 print(line) # Prints the read line
@@ -22,8 +22,43 @@ file.close()
 # Appending to a text file:
 
 # Open a file in append mode ('a')
-file = open( 'output. txt', 'a' )
+file = open('output.txt', 'a' )
 # Append a line of text to the file
-file write("\nThis is another line of text.")
+file.write("\nThis is another line of text.")
 # Close the file
 file.close()
+
+
+
+# Write Multiple Lines
+lines = ["Line 1\n", "Line 2\n", "Line 3\n"]
+# with open("example.txt", "w") as file:
+#     file.writelines(lines)
+
+# OR
+with open("example.txt", "w") as file:
+    for line in lines:
+        file.write(line)
+
+
+# Append Multiple Lines
+more_lines = ["Line 4\n", "Line 5\n"]
+# with open("example.txt", "a") as file:
+#     file.writelines(more_lines)
+
+# OR
+with open("example.txt", "a") as file:
+    for line in more_lines:
+        file.write(line)
+
+
+# Read Multiple Lines
+with open('output.txt', 'r') as file:
+    for line in file:
+        print(line)
+
+# OR
+with open("example.txt", "r") as file:
+    for line in file:
+        print(line.strip()) 
+        # .strip() removes any leading and trailing whitespace (including \n) from a string.
